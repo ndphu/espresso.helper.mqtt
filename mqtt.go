596a55f7707fed1ba25a18d37f22e57a88327fab
Phu Nguyen <ndphu@localhost.com>
2016-09-13 17:55:39 +0700

    update api

--- mqtt.go
+++ mqtt.go
@@ -5,22 +5,24 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	appconfig "github.com/ndphu/espresso.appconfig"
 	"strconv"
+	"time"
 )
 
-type mqttApplication struct {
-	AppConfig *appconfig.AppConfig
-	Opts      *mqtt.ClientOptions
-}
-
-func (mqttApp *mqttApplication) Init(appConfig *appconfig.AppConfig) {
-	mqttApp.Opts = CreateClientOpts(appConfig)
-}
-
-func CreateClientOpts(appConfig *appconfig.AppConfig) *mqtt.ClientOptions {
+func NewClientOpts(appConfig *appconfig.AppConfig) *mqtt.ClientOptions {
 	brokerUrl := fmt.Sprintf(appConfig.Server.MQTT.Protocol + "://" + appConfig.Server.MQTT.Host + ":" + strconv.Itoa(appConfig.Server.MQTT.Port))
 	opts := mqtt.NewClientOptions().AddBroker(brokerUrl)
 	opts.SetUsername(appConfig.Server.MQTT.User)
 	opts.SetPassword(appConfig.Server.MQTT.Password)
 	opts.SetClientID(appConfig.Device.Id)
 	return opts
+}
+
+func NewClient(conf *appconfig.AppConfig) mqtt.Client {
+	opts := NewClientOpts(conf)
+	opts.SetClientID(conf.Device.Id + fmt.Sprintf("%d", time.Now().Nanosecond()))
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	return client
 }
--- mqtt_test.go
+++ mqtt_test.go
@@ -5,15 +5,19 @@ import (
 	"github.com/ndphu/espresso.appconfig"
 	mqtt_helper "github.com/ndphu/espresso.helper.mqtt"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 var (
-	TEST_HOST     = "somewhere.far.far.far.away"
-	TEST_PORT     = 1990
-	TEST_USERNAME = "tester"
-	TEST_PASSWORD = "secret"
-	TEST_PROTOCOL = "tcp"
+	TEST_HOST              = "somewhere.far.far.far.away"
+	TEST_PORT              = 1990
+	TEST_USERNAME          = "tester"
+	TEST_PASSWORD          = "secret"
+	TEST_PROTOCOL          = "tcp"
+	TEST_FIREBASE_APP_NAME = "test-7a4ff"
+	TEST_DEVICE_ID         = "0"
+	TEST_KEY_FILE          = "key.json"
 )
 
 func TestCreateClientOpts(t *testing.T) {
@@ -23,9 +27,48 @@ func TestCreateClientOpts(t *testing.T) {
 	conf.Server.MQTT.User = TEST_USERNAME
 	conf.Server.MQTT.Password = TEST_PASSWORD
 	conf.Server.MQTT.Protocol = TEST_PROTOCOL
-	opts := mqtt_helper.CreateClientOpts(conf)
+	opts := mqtt_helper.NewClientOpts(conf)
 	expectedBrokerUrl := fmt.Sprintf("%s://%s:%d", TEST_PROTOCOL, TEST_HOST, TEST_PORT)
 	assert.Equal(t, expectedBrokerUrl, opts.Servers[0].String(), "Broker URL mismatch")
 	assert.Equal(t, TEST_USERNAME, opts.Username, "Username mismatch")
 	assert.Equal(t, TEST_PASSWORD, opts.Password, "Password mismatch")
+}
+
+func GetTestFirebaseAppName() string {
+	firebaseAppName, exists := os.LookupEnv("FIREBASE_APP")
+	if !exists {
+		return TEST_FIREBASE_APP_NAME
+	}
+	return firebaseAppName
+}
+
+func GetTestDeviceId() string {
+	deviceId, exists := os.LookupEnv("DEVICE_ID")
+	if !exists {
+		return TEST_DEVICE_ID
+	}
+	return deviceId
+}
+func GetTestKeyFile() string {
+	keyFile, exists := os.LookupEnv("KEY_FILE")
+	if !exists {
+		return TEST_KEY_FILE
+	}
+	return keyFile
+}
+
+func TestCreateClientOptsFromFirebase(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping test in short mode.")
+	}
+	conf := appconfig.New()
+	conf.GetConfigFromFirebase(GetTestFirebaseAppName(), GetTestDeviceId(), GetTestKeyFile())
+
+	client := mqtt_helper.NewClient(conf)
+
+	assert.True(t, client.IsConnected(), "Client is not connected")
+
+	//conf := appconfig.New()
+	//conf.GetConfigFromFirebase(deviceId, authToken)
+
 }
